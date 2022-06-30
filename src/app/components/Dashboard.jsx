7cb0225b9529fde2268993b879c12530f27b0405
Chisambwe Kapihya <cmkap@chisambwes-mbp.home>
2022-06-30 18:36:48 +0100

    Added 'Addnew' button, updated each child in Dashboard and Tasklist to have a unique key prop

--- src/app/components/Dashboard.jsx
+++ src/app/components/Dashboard.jsx
@@ -6,7 +6,7 @@ export const Dashboard = ({groups}) => (
   <div>
     <h2>Dashboard</h2>
     {groups.map(group =>(
-        <ConnectedTaskList  id={group.id} name={group.name}/>
+        <ConnectedTaskList key={group.id } id={group.id} name={group.name}/>
     ))}
   </div>  
 ) // not curly brackets to suggest it is not a function but an object to be returned
