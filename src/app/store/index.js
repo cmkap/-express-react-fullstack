4512cc2f454a7f84b2b5ae5721bf97bfe028c229
Chisambwe Kapihya <c.m.kapihya@gmail.com>
2022-07-02 23:51:48 +0100

    Added Mock Saga

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -1,10 +1,18 @@
 import { createStore, applyMiddleware } from 'redux';
 import { defaultState } from '../../server/defaultState';
 import { createLogger } from 'redux-logger';
+import createSagaMiddleware from 'redux-saga';
+
+const sagaMiddleWare = createSagaMiddleware();
+import * as sagas from './sagas.mock';
 
 export const store = createStore (
     function reducer(state = defaultState ,action){
         return state;
     },
-    applyMiddleware(createLogger())
-)
+    applyMiddleware(createLogger(), sagaMiddleWare)
+);
+
+for (let saga in sagas) {
+    sagaMiddleWare.run(sagas[saga])
+}
