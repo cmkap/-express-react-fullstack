a914d5546e678e77435135633671033fc9c77a7d
Chisambwe Kapihya <c.m.kapihya@gmail.com>
2022-07-12 16:00:40 +0100

    Combined Ruducer updated,  Set Task Complete functional and du isplaying 'Complete' and 'Reopen tas k'

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -21,7 +21,10 @@ export const store = createStore (
                        owner:action.ownerID,
                        isComplete: false
                     }]
-
+                case mutations.SET_TASK_COMPLETE:
+                    return tasks.map(task=>{
+                        return (task.id === action.taskID) ? {...task,isComplete:action.isComplete} : task;
+                    })
             }
             return tasks;
         },
